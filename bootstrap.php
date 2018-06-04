76cd29ff9d0e636f2f4a24e0116d33600fb6a812
jlchassaing <jlchassaing@gmail.com>
2018-06-04 15:35:15 +0200

    add codecov

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
-$autoloadFile = __DIR__.'/vendor/autoload.php';
+$autoloadFile = realpath(__DIR__.'/../../autoload.php');
+
 if (!file_exists($autoloadFile)) {
     throw new RuntimeException('Install dependencies to run test suite.');
 }
